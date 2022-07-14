9d0bfd7d3a4d2508d5dde6bd3e24dc7510f0f73b
Catalin Ciobanu <ciobanu.m.catalin@gmaill.com>
2022-07-14 21:16:43 +0300

    Added liting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import {photos} from "./data/photos";
-import {Gallery} from './components';
+import { photos } from "./data/photos";
+import { Gallery } from "./components";
 import "./styles.scss";
 
 export default function App() {
-
-    return (<Gallery photos={        photos}/>
-    );
+  return <Gallery photos={photos} />;
 }
--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,29 +1,29 @@
-import {FC, useState} from "react";
+import { FC, useState } from "react";
 
 interface PropsInterface {
-    photos: any[],
+  photos: any[];
 }
 
-const Gallery: FC<PropsInterface> = ({photos}) => {
-    const [size, setSize] = useState("thumb");
-    const [selected, setSelected] = useState(null);
+const Gallery: FC<PropsInterface> = ({ photos }) => {
+  const [size, setSize] = useState("thumb");
+  const [selected, setSelected] = useState(null);
 
-    return (
-        <div className="App">
-            <h3>Photos courtesy of Unsplash and it's users</h3>
-            <div className="container">
-                <div className="">
-                    {photos.map((p) => (
-                        <div className="display-flex" key={p.id}>
-                            <button className="btn">
-                                <img src={p.urls[size]} alt={`Taken by ${p.user.name}`} />
-                            </button>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div className="App">
+      <h3>Photos courtesy of Unsplash and it's users</h3>
+      <div className="container">
+        <div className="">
+          {photos.map((p) => (
+            <div className="display-flex" key={p.id}>
+              <button className="btn">
+                <img src={p.urls[size]} alt={`Taken by ${p.user.name}`} />
+              </button>
             </div>
+          ))}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
-export {Gallery}
+export { Gallery };
