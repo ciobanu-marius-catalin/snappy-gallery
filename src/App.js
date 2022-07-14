95de3d092cfff7c1711f7259ba8485c2c66dfa81
Catalin Ciobanu <ciobanu.m.catalin@gmaill.com>
2022-07-14 20:39:56 +0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {photos} from "./data/photos";
+import {Gallery} from './components';
+import "./styles.scss";
+
+export default function App() {
+
+    return (
+            <Gallery photos={photos}/>
+    );
+}
---
+++ src/components/gallery/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+
+const Gallery = ({photos}) => {
+    const [size, setSize] = useState("thumb");
+    const [selected, setSelected] = useState(null);
+
+    return (
+        <div className="App">
+            <h3>Photos courtesy of Unsplash and it's users</h3>
+            <div className="container">
+                <div className="">
+                    {photos.map((p) => (
+                        <div className="display-flex" key={p.id}>
+                            <button className="btn">
+                                <img src={p.urls[size]} alt={`Taken by ${p.user.name}`} />
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export {Gallery}
