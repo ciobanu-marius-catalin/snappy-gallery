5fdaeeeac7b11b6944c8ff45c051785d8cbd0e2d
Catalin Ciobanu <ciobanu.m.catalin@gmaill.com>
2022-07-18 00:29:09 +0300

    Added some comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { photosWithSrcSet } from "./data/photos";
 import { Gallery } from "./components";
 import "./styles.scss";
 
+/*
+   There can be made extra improvements to the applications.
+   - We can use a virtual list to only display a set number of elements that are in the viewport.
+   - We can add better transition for enter/exit lightbox. To work like the gallery application from android for example
+   - In the lightbox we can add a preview list at the bottom that can be scrollable to select images from there
+ */
 export default function App() {
   return <Gallery photos={photosWithSrcSet} />;
 }
